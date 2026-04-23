2a7dd9200536bb33426c2929325d65bb08dea710
AnshumanDave <87855120+AnshumanDave@users.noreply.github.com>
2026-04-23 07:15:01 +0530

    Enhance testbench with VCD dump configuration
    Added VCD dump setup for waveform analysis.

--- pipeline_core/src/tb_riscv_sc.v
+++ pipeline_core/src/tb_riscv_sc.v
@@ -5,18 +5,21 @@ reg clk;
 reg start;
 wire [3:0] out_1;
 
-CompleteCPU riscv_DUT(clk,start,out_1);
+CompleteCPU riscv_DUT(clk, start, out_1);
 
 initial
-	forever #5 clk = ~clk;
+    forever #5 clk = ~clk;
 
 initial begin
-	clk = 0;
-	start = 0;
-	#10 start = 1;
+    // VCD dump setup
+    $dumpfile("pipeline_wave.vcd");
+    $dumpvars(0, tb_riscv_sc);
 
-	#3000 $finish;
+    clk = 0;
+    start = 0;
+    #10 start = 1;
 
+    #3000 $finish;
 end
 
 endmodule
